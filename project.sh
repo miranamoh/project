2b2fd7f13fee173e46d190abf577987a4a70d3a8
ghadeer <vboxuser@ubuntu.myguest.virtualbox.org>
2025-05-20 04:52:57 +0300

    Ghadeer commit

--- project.sh
+++ project.sh
@@ -51,4 +51,55 @@ case $budget_choice in
 esac
 
 echo "Fetching suggestions for you..."
+<<<<<<< HEAD
 echo "--------------------------------"
+=======
+echo "-------------------------------—-"
+
+##___
+
+case $goal_lower in
+  "education")
+
+    read -p "Type of education (School/Higher): " edu_type
+    
+ # Convert input to lowercase
+    edu_type_lower=$(echo "$edu_type" | tr '[:upper:]' '[:lower:]')
+
+  # Validate eduType & set corresponding section header
+    if [[ "$edu_type_lower" == "school" ]]; then
+      section="## Type: School Education"
+    elif [[ "$edu_type_lower" == "higher" ]]; then
+      section="## Type: Higher Education"
+    else
+      echo "Invalid type."
+      exit 1
+    fi
+
+   ## Loop through data file & filter lines matching the selected section and budget
+    print=0
+    while read -r line; do
+  # Start print when the section matches
+      echo "$line" | grep -q "^$section$" && print=1 && continue
+      
+   #Stop printing if another section start
+      echo "$line" | grep -q "^##" && print=0
+
+     #If in the right section and the line is not empty
+      if [ $print -eq 1 ] && [ -n "$line" ]; then
+    #Extract the budget part from line
+        dest_budget=$(echo "$line" | sed 's/.*- *//')
+        
+ ##If budget matches, extract and print the destination name
+     if [ "$dest_budget" = "$budget" ]; then
+          dest_name=$(echo "$line" | sed 's/-.*//')
+          echo "- $dest_name"
+fi
+fi
+    done < "$DATA_FILE"
+    ;;
+esac
+
+
+
+>>>>>>> a18be0a (Ghadeer commit)
